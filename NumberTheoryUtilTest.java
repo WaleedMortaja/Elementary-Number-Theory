f4f35df1e75bc02990d22813de4bd1f9fc051d23
محمود الشيخ خليل <kkhalil2535@gmail.com>
2017-11-23 17:42:15 +0200

    Update NumberTheoryUtilTest.java

--- NumberTheoryUtilTest.java
+++ NumberTheoryUtilTest.java
@@ -1,11 +1,12 @@
+package number.theory;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package newpackage;
-
 import java.util.ArrayList;
+import number.theory.NumberTheoryUtil;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -89,7 +90,7 @@ public class NumberTheoryUtilTest {
      * Test of gcd method, of class NumberTheoryUtil.
      */
     @Test
-    public void testGcd() {
+    public void testGcd_long_long() {
         System.out.println("gcd");
         long a = 1000L;
         long b = -7L;
@@ -228,6 +229,45 @@ public class NumberTheoryUtilTest {
     }
 
     /**
+     * Test of gcdAsLinearCombination method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test2GcdAsLinearCombination() {
+        System.out.println("2 gcdAsLinearCombination");
+        long a = 10L;
+        long b = 5L;
+        long[] expResult = {5, 1, 5, 0, 10};
+        long[] result = NumberTheoryUtil.gcdAsLinearCombination(a, b);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of gcdAsLinearCombination method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test3GcdAsLinearCombination() {
+        System.out.println("3 gcdAsLinearCombination");
+        long a = -10L;
+        long b = 5L;
+        long[] expResult = {5, 1, 5, 0, -10};
+        long[] result = NumberTheoryUtil.gcdAsLinearCombination(a, b);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of gcdAsLinearCombination method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test4GcdAsLinearCombination() {
+        System.out.println("4 gcdAsLinearCombination");
+        long a = -3L;
+        long b = 2L;
+        long[] expResult = {1, 1, - 3, 2, 2};
+        long[] result = NumberTheoryUtil.gcdAsLinearCombination(a, b);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
      * Test of primeFacorization method, of class NumberTheoryUtil.
      */
     @Test
@@ -240,5 +280,118 @@ public class NumberTheoryUtilTest {
         expResult.add("7");
         ArrayList<String> result = NumberTheoryUtil.primeFacorization(number);
         assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of mod method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void testMod() {
+        System.out.println("mod");
+        long a = -20L;
+        long n = 7L;
+        long expResult = 1L;
+        long result = NumberTheoryUtil.mod(a, n);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of gcd method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void testGcd_ArrayList() {
+        System.out.println("gcd_ArrayList");
+        ArrayList<Long> array = new ArrayList<>();
+        array.add(84651L);
+        array.add(651L);
+        array.add(854L);
+        long expResult = 7;
+        long result = NumberTheoryUtil.gcd(array);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of gcd method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test2Gcd_ArrayList() {
+        System.out.println("2 gcd_ArrayList");
+        ArrayList<Long> array = new ArrayList<>();
+        array.add(84651L);
+        array.add(651L);
+        long expResult = 21;
+        long result = NumberTheoryUtil.gcd(array);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of gcdLines method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void testGcdLines() {
+        System.out.println("gcdLines");
+        long a = 172L;
+        long b = 42L;
+        ArrayList expResult = new ArrayList();
+        expResult.add(new long[]{172, 4, 42, 4});
+        expResult.add(new long[]{42, 10, 4, 2});
+        expResult.add(new long[]{4, 2, 2, 0});
+        ArrayList result = NumberTheoryUtil.gcdLines(a, b);
+        for (int i = 0; i < result.size(); i++) {
+            assertArrayEquals((long[]) expResult.get(i), (long[]) result.get(i));
+        }
+    }
+
+    /**
+     * Test of gcdLines method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test2GcdLines() {
+        System.out.println("2 gcdLines");
+        long a = -3L;
+        long b = 2L;
+        ArrayList expResult = new ArrayList();
+        expResult.add(new long[]{-3, -2, 2, 1});
+        expResult.add(new long[]{2, 2, 1, 0});
+        ArrayList result = NumberTheoryUtil.gcdLines(a, b);
+        for (int i = 0; i < result.size(); i++) {
+            assertArrayEquals((long[]) expResult.get(i), (long[]) result.get(i));
+        }
+    }
+
+    /**
+     * Test of gcdLines method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test3GcdLines() {
+        System.out.println("3 gcdLines");
+        long a = 10L;
+        long b = 5L;
+        ArrayList expResult = new ArrayList();
+        expResult.add(new long[]{10, 2, 5, 0});
+        ArrayList result = NumberTheoryUtil.gcdLines(a, b);
+        for (int i = 0; i < result.size(); i++) {
+            assertArrayEquals((long[]) expResult.get(i), (long[]) result.get(i));
+        }
+
+    }
+
+    /**
+     * Test of gcdLines method, of class NumberTheoryUtil.
+     */
+    @Test
+    public void test4GcdLines() {
+        System.out.println("4 gcdLines");
+        long a = 4L;
+        long b = 5L;
+        ArrayList expResult = new ArrayList();
+        expResult.add(new long[]{4, 0, 5, 4});
+        expResult.add(new long[]{5, 1, 4, 1});
+        expResult.add(new long[]{4, 4, 1, 0});
+        ArrayList result = NumberTheoryUtil.gcdLines(a, b);
+        for (int i = 0; i < result.size(); i++) {
+            assertArrayEquals((long[]) expResult.get(i), (long[]) result.get(i));
+        }
+
     }
 }
